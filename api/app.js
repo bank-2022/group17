dc23f03252927747fffbcd271e6fe196975daf4e
MiniWalls <69449726+MiniWalls@users.noreply.github.com>
2022-03-30 12:12:39 +0300

    Added express database connection and model for table tili

--- api/app.js
+++ api/app.js
@@ -4,9 +4,17 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var tiliRouter = require('./routes/tili');
+var tilitapahtumatRouter = require('./routes/tilitapahtumat');
 
-var app = express();
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+var app = express(); 
+
+app.use(helmet());
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/tili', tiliRouter);
+app.use('/tilitapahtumat', tilitapahtumatRouter);
 
 module.exports = app;
