9c380b15690d6c343f66f22f3d535e8ec9addf10
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-04-04 13:43:19 +0300

    tekninen määrittely siirretty oikeaan kansioon

--- api/database.js
+++ api/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
-  user: 'netuser',
-  password: 'netpass',
+  user: 'root',
+  password: 'ROOT',
   database: 'banksimuldb'
 });
 module.exports = connection;
