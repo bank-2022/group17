bb338a23979f47b68d6abc157475916201c79599
Ilari Tuovinen <99398849+TuovinenIlari@users.noreply.github.com>
2022-04-04 13:44:50 +0300

    Delete database.js

--- api/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'ROOT',
-  database: 'banksimuldb'
-});
-module.exports = connection;
