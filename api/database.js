8311da29f6bcdffd9a1f85322bb04bb97fcea71c
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-04-04 13:53:51 +0300

    dokumentit korjattu ja database.js yhdenmukaistettu

--- api/database.js
+++ api/database.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'ROOT',
+  user: 'netuser',
+  password: 'netpass',
   database: 'banksimuldb'
 });
 module.exports = connection;
