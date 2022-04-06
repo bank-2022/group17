492f7783cfb70de555c0511f7d62e38d0181ac84
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2022-04-06 15:41:57 +0300

    UI nappien toiminnallisuuksia alustettu

--- bankautomat/bankui.cpp
+++ bankautomat/bankui.cpp
@@ -6,9 +6,47 @@ BankUI::BankUI(QWidget *parent) :
     ui(new Ui::BankUI)
 {
     ui->setupUi(this);
+
 }
 
 BankUI::~BankUI()
 {
     delete ui;
 }
+
+void BankUI::on_NostaBtn_clicked()
+{
+    pNostaWindow = new NostaWindow;
+    pNostaWindow->setModal(true);
+    pNostaWindow->exec();
+}
+
+
+void BankUI::on_TalletaBtn_clicked()
+{
+    pTalletaWindow = new TalletaWindow;
+    pTalletaWindow->setModal(true);
+    pTalletaWindow->exec();
+}
+
+
+void BankUI::on_TapahtumatBtn_clicked()
+{
+    //get tapahtumat
+}
+
+
+void BankUI::on_PoistuBtn_clicked()
+{
+    this->close();
+}
+
+
+void BankUI::on_SaldoBtn_clicked()
+{
+    //get saldo
+    getsaldo=450.89;
+    saldo = QString::number(getsaldo);
+    ui->saldo_label->setText(saldo+" €");
+}
+
--- bankautomat/mainwindow.h
+++ bankautomat/mainwindow.h
@@ -1,17 +1,32 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "bankui.h"
 #include <QMainWindow>
 #include <QTimer>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 enum states{
-
+    Aloitus,    //Ohjelman vakiotila
+    Luekortti,
+    Pankissa,   //Kun pankkiin on päästy
+    Nosta,      //
+    Talleta,
+    Tilitapahtumat,
+    Saldo,
+    Poistu
 };
 enum events{
+    avaaRFID,
+    luekortti,
+    suljeRFID,
+    pinKoodi,
+
+
 
 };
 class MainWindow : public QMainWindow
@@ -22,8 +37,14 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_LuekorttiBtn_clicked();
+
+    void on_AnnaPinBtn_clicked();
+
 private:
     Ui::MainWindow *ui;
+    BankUI *pBankUI;
 
     QString CardNum;
     QString CardPin;
