a1f27e6c822f9a9f9e6ddd693dd7dcd729b30c1f
Ilari Tuovinen <99398849+TuovinenIlari@users.noreply.github.com>
2022-04-21 12:04:57 +0300

    Delete api directory

--- api/models/login_model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database');
-
-const login={
-  checkPin: function(idKortti, callback) {
-      return db.query('SELECT pin FROM kortti WHERE idKortti = ?',[idKortti], callback); 
-    }
-};
-          
-module.exports = login;
